3cd55e762ddadd620fe5a3df4d745721cde196e6
Dawn <dawnnyzxc@gmail.com>
2019-01-21 12:31:50 +0800

    First commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_P5llbyzET', // on general settings (very first page)
+        region: 'ap-southeast-1', // e.g. us-east-2
+		clientId: 'lhmbpv49gs52ae1h3quoassp6' // under app integration
+    },
+};
